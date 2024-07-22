77cba4f9e04ef27d394e84c33d1c9d4a2c317d3a
klmadhu247 <klmadhu0709@gmail.com>
2024-07-22 21:35:03 +0530

    80%

--- src/budget.js
+++ src/budget.js
@@ -1,19 +1,74 @@
 import React,{useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './budget.css'
+import {RemoveScrollBar} from 'react-remove-scroll-bar';
+
 import Logo from './images/Logo.png' 
 
 function Budget (){
 
+    const [purchaseList,setPurchaseList] = useState([])
 
-    const [budgetValue,setBudgetValue] = useState(2000)
+    const [newItem,setNewItem] = useState({id:0,name:'',amount:0})
+
+
+    const [budgetValue,setBudgetValue] = useState(parseInt (2000))
 
     const euro = '£';
 
-    const [remaining,setRemaining] = useState(2000)
+    const [remaining,setRemaining] = useState(parseInt(budgetValue))
 
     const [spent,setSpent] = useState(0)
 
+    const [expName,setExpName] = useState('')
+
+    const [cost,setCost] = useState(0 )
+    
+    const [totalcost,setTotalCost] = useState(0)
+   
+    const handleName =(e)=>
+    {
+        setExpName(e.target.value)
+    }
+    const handleCost =(e)=>
+    {
+        setCost(parseInt(e.target.value))
+    }
+    
+
+    const handleAdd =()=>
+    {
+        const newTotalCost = purchaseList.reduce((prev,current)=> prev+current,0)
+        setTotalCost(newTotalCost)
+        setSpent(newTotalCost)
+        setRemaining(remaining-cost)
+        
+
+        const newId = purchaseList.length ? purchaseList[purchaseList.length - 1].id + 1 : 0;
+    const updatedItem = { id: newId, name: expName, amount: cost }
+    setPurchaseList([...purchaseList, updatedItem])
+    setCost(0)
+    
+    
+    
+        console.log(newTotalCost)
+        console.log(purchaseList)
+      
+
+    }
+    const handleEdit =()=>
+    {
+        const updatedBudget = prompt('Update the Budget here', budgetValue)
+        if(updatedBudget)
+        {
+            setBudgetValue(parseInt(updatedBudget))
+            setRemaining(parseInt(updatedBudget))
+            
+        }
+
+    }
+
+    
 
 
 
@@ -26,9 +81,9 @@ function Budget (){
     return(
         <div>
             
-           <div className="conatainer-fluid">
+           <div className="container-fluid">
            <div className=" d-flex  ">
-           <div className="col-12 col-md-6 d-flex flex-column align-items-center bg-custom">
+           <div className="col-12 col-md-5 d-flex flex-column align-items-center bg-custom">
                 <h1 className="heading pb-4">
                     <span className="blue-color">REACT</span>
                 <span className="white-color"> BUDGET <br/> APP</span></h1>
@@ -39,7 +94,8 @@ function Budget (){
             <div>
                 <header className="ms-2 hdr"><h2>My Budget Planner</h2></header>
                 <ul className="d-flex ms-2 ">
-                    <li className="budget">Budget: {euro}{budgetValue}<button className="btn btn-primary btn-budget ">Edit</button> </li>
+                    <li className="budget">Budget: {euro}{budgetValue}
+                        <button onClick={handleEdit} className="btn btn-primary btn-budget ">Edit</button> </li>
                     <li className="remaining">Remaining: {euro} {remaining} </li>
                     <li className="spent">Spent so far:{euro} {spent} </li>
                 </ul>
@@ -54,16 +110,16 @@ function Budget (){
 
                     <div className="form-group col-md-6 col-12">
                         <label className="form-label">Name</label>
-                        <input className="form-control"/>
+                        <input value={expName} onChange={handleName} className="form-control"/>
 
                     </div>
                     <div className="form-group col-md-5 col-12">
                         <label className="form-label">Cost</label>
-                        <input className="form-control" type="number" />
+                        <input value={cost} onChange={handleCost} className="form-control" type="number" />
                        
                     </div>
                     <div className="col-md-1 d-flex align-items-end">
-        <button className="btn btn-primary w-150">Add</button>
+        <button onClick={handleAdd} className="btn btn-primary w-150">Add</button>
       </div>
                     
                     
@@ -78,6 +134,7 @@ function Budget (){
 
 
            </div>
+           {/* <RemoveScrollBar/> */}
         </div>
     )
 }
